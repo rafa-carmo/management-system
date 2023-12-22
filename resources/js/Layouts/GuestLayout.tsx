eae7e5f12a4d44b00a11cca8dc9850cb9955c919
rafa-carmo <rafaelcarmo143@gmail.com>
2023-12-22 01:07:08 -0300

    biome js script config

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -1,21 +1,20 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
-import { PropsWithChildren } from 'react';
-import { MdManageAccounts } from "react-icons/md";
-
+import ApplicationLogo from '@/Components/ApplicationLogo'
+import { Link } from '@inertiajs/react'
+import { PropsWithChildren } from 'react'
+import { MdManageAccounts } from 'react-icons/md'
 
 export default function Guest({ children }: PropsWithChildren) {
-    return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <Link href="/">
-                    <MdManageAccounts className="w-44 h-44 fill-current text-gray-500" />
-                </Link>
-            </div>
+	return (
+		<div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+			<div>
+				<Link href="/">
+					<MdManageAccounts className="w-44 h-44 fill-current text-gray-500" />
+				</Link>
+			</div>
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {children}
-            </div>
-        </div>
-    );
+			<div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+				{children}
+			</div>
+		</div>
+	)
 }
